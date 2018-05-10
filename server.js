f18df110ff08344513136b3ba61bd213a21d579f
Brita Ramsay <britaramsay@gmail.com>
2018-05-10 15:18:45 -0400

    dyanmic port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express"),
 
 // Sets up the Express App
 var app = express(),
-    port = 3000;
+    port = process.env.PORT || 3000;
     
 
 // Sets up the Express app to handle data parsing
