00d878f73d41933d84b5d156883bb0d460ea92f7
Brita Ramsay <britaramsay@gmail.com>
2018-05-10 16:15:40 -0400

    added css

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const express = require("express"),
 var app = express(),
     port = process.env.PORT || 3000;
     
-
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname + '/app/public')));
 
 var htmlRoutes = require('./app/routing/htmlRoutes.js')(app, path),
     apiRoutes = require('./app/routing/apiRoutes.js')(app, path); 
 
+    
 // Starts the server to begin listening
 app.listen(port, () => console.log("App listening on PORT " + port));
