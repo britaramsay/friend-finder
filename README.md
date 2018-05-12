be62ca5fffaa87652f468c22cb7edb897fa381f7
Brita Ramsay <britaramsay@gmail.com>
2018-05-11 20:51:35 -0400

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # friend-finder
+[Use the app](https://shrouded-atoll-60290.herokuapp.com/)
+---
+* This express app finds your most compatible friend based on your choice of TV shows.
+* Complete the survey by rating the shows listed from 1(horrible) to 5(great).
+---
+#### Tools used in this project
+* [npm express](https://www.npmjs.com/package/express)
+* [npm body-parser](https://www.npmjs.com/package/body-parser)
+* [npm path](https://www.npmjs.com/package/path)
--- server.js
+++ server.js
@@ -10,11 +10,11 @@ var app = express(),
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname + '/app/public')));
 
 var htmlRoutes = require('./app/routing/htmlRoutes.js')(app, path),
     apiRoutes = require('./app/routing/apiRoutes.js')(app, path); 
-
     
 // Starts the server to begin listening
 app.listen(port, () => console.log("App listening on PORT " + port));
